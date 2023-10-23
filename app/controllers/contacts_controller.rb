683407138c007b5f05f4c81f809c11f210a9e2be
brandonsnover <144728738+brandonsnover@users.noreply.github.com>
2023-10-23 13:49:46 -0500

    Initial commit

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,17 @@
+class ContactsController < ApplicationController
+  def one_contact
+    contact = Contact.first
+    render json: {
+      id: contact.id,
+      first_name: contact.first_name,
+      last_name: contact.last_name,
+      email: contact.email,
+      phone_number: contact.id,
+    }
+  end
+
+  def all_contacts
+    contact = Contact.all
+    render json: { list: contact }
+  end
+end
